d4e76e73f068eb2a668014432c9f111ec4624734
prodanserega <74560554+prodanserega@users.noreply.github.com>
2021-06-05 13:22:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,50 +7,50 @@ class Timer {
   }
 
   start() {
+    const currentTime = Date.now();
+    const time = this.targetDate - currentTime;
+    const { days, hours, mins, secs } = this.getTimeComponents(time);
+    this.updateTimeFields({ days, hours, mins, secs });
     setInterval(() => {
       const currentTime = Date.now();
       const time = this.targetDate - currentTime;
       const { days, hours, mins, secs } = this.getTimeComponents(time);
-      this.updateTimeFields({ days, hours, mins, secs });
-    }, 1000);
+      this.updateTimeFields({ days, hours, mins, secs });}, 1000);
   }
 
-getFieldsBySelector(selector) {
-  const refs = {
-    days: document.querySelector(`${selector} [data-value="days"]`),
-    hours: document.querySelector(`${selector} [data-value="hours"]`),
-    mins: document.querySelector(`${selector} [data-value="mins"]`),
-    secs: document.querySelector(`${selector} [data-value="secs"]`),
-  };
+  getFieldsBySelector(selector) {
+    const refs = {
+      days: document.querySelector(`${selector} [data-value="days"]`),
+      hours: document.querySelector(`${selector} [data-value="hours"]`),
+      mins: document.querySelector(`${selector} [data-value="mins"]`),
+      secs: document.querySelector(`${selector} [data-value="secs"]`),
+    };
 
-  return refs;
-}
+    return refs;
+  }
 
-updateTimeFields({ days, hours, mins, secs }) {
-  this.time.days.textContent = days;
-  this.time.hours.textContent = hours;
-  this.time.mins.textContent = mins;
-  this.time.secs.textContent = secs;
-}
+  updateTimeFields({ days, hours, mins, secs }) {
+    this.time.days.textContent = days;
+    this.time.hours.textContent = hours;
+    this.time.mins.textContent = mins;
+    this.time.secs.textContent = secs;
+  }
 
+  getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
 
-getTimeComponents(time) {
-  const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-  const hours = this.pad(
-    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-  );
-  const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-  const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    return { days, hours, mins, secs };
+  }
 
-  return { days, hours, mins, secs };
-}
-
-pad(value) {
-  return String(value).padStart(2, '0');
-}
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
 }
 
 new Timer({
-selector: '#timer-1',
-targetDate: new Date('Aug 31, 2021'),
+  selector: '#timer-1',
+  targetDate: new Date('Aug 31, 2021'),
 });
